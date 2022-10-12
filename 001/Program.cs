c1a996e1492fa37aa65f16c744a4af12d1368f0d
bigdi <bigdi@tak.tak>
2022-10-12 21:28:04 +0300

    the solution

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу которая выводит массив из строк, длина который меньше либо равна 3. 
+
+
+string[] array = new string[6] { "1", "asdasd", "1234", "-2", "hello", "hel" };
+
+void PrintLessFour(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+
+}
+PrintLessFour(array);
